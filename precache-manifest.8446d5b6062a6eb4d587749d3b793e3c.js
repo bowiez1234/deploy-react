633fdf32cb370cfc1941ead7ebfe5886929e6697
bowiez1234 <bowiez1234@gmail.com>
2020-06-04 19:57:57 +0700

    Updates

---
+++ precache-manifest.8446d5b6062a6eb4d587749d3b793e3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e04d87dcf0f06a939421dae5de9f2abe",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "6ff3bb223e0ef0908386",
+    "url": "/deploy-react/static/css/main.7e9225ec.chunk.css"
+  },
+  {
+    "revision": "64307e05c8bec69fa50a",
+    "url": "/deploy-react/static/js/2.d42e7c4f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-react/static/js/2.d42e7c4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ff3bb223e0ef0908386",
+    "url": "/deploy-react/static/js/main.835a915c.chunk.js"
+  },
+  {
+    "revision": "8c244ad6dead24b32ae6",
+    "url": "/deploy-react/static/js/runtime-main.373d5be3.js"
+  }
+]);
